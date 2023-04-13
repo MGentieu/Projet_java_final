e6ba15961be457c222e7ca9687197b681ad13202
MGentieu <mgentieu02@gmail.com>
2023-04-14 01:06:25 +0200

    Commitment

--- src/BDD_connexion/Test_Connexion.java
+++ src/BDD_connexion/Test_Connexion.java
@@ -7,11 +7,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Test_Connexion {
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
         Utilisateurs u = new Utilisateurs();
         Livre l1 = new Livre(1);
-        Livre l2 = new Livre(1);
+        Livre l2 = new Livre(2);
 
         System.out.println(l1.toString());
         System.out.println(l2.toString());
@@ -19,17 +19,17 @@ public class Test_Connexion {
         try {
             // Création d'une nouvelle connexion à la base de données
             Connexion connexion = new Connexion("ece_shopping_4", "root", "");
-            u = connexion.recherche_login("maxime.ambroise@edu.ece.fr","maxime05");
-            u.ajouterPanier(l1,1);
-            u.ajouterPanier(l2,2);
+            u = connexion.recherche_login("maxime.ambroise@edu.ece.fr", "maxime05");
+            u.ajouterPanier(l1, 1);
+            u.ajouterPanier(l2, 1);
             //u.validerPanier();
             String requete = "UPDATE livre SET stock = 3 WHERE identifiant = 1;";
             connexion.executeUpdate(requete);
             requete = "UPDATE livre SET stock = 3 WHERE identifiant = 2;";
             connexion.executeUpdate(requete);
             u.viderPanier();
-            System.out.println("\n"+u.getL_nb_achats().size()+"    "+u.getMonPanier().size());
-            System.out.println("\n\n"+l1.toString());
+            System.out.println("\n" + u.getL_nb_achats().size() + "    " + u.getMonPanier().size());
+            System.out.println("\n\n" + l1.toString());
             System.out.println(l2.toString());
             // System.out.println(u.toString());
             connexion.close();
@@ -37,6 +37,8 @@ public class Test_Connexion {
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+}
 
         /*ArrayList<Livre> liste = new ArrayList<Livre>();
 
@@ -89,8 +91,6 @@ public class Test_Connexion {
             System.out.println(liste.get(0).recherche_Livre_stock());
 
         }*/
-    }
-}
 /*
 import java.sql.Connection;
         import java.sql.DriverManager;
